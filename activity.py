d2e6d8f6a964d82626da701fc3093061a47dadc0
jahu1251 <jahuswiesu@gmail.com>
2022-03-18 13:02:57 +0100

    list repaired

--- activity.py
+++ activity.py
@@ -1,7 +1,7 @@
 class activity:
-    originEvent = 0
-    predecessors = []
     def __init__(self, name, duration, predecessor):
+        self.originEvent = 0
+        self.predecessors = []
         self.name = name
         self.duration = duration
         if predecessor == '':#jesli czynnosc nie ma poprzednika, to wychodzi zawsze z zdarzenia o ID = 1
--- event.py
+++ event.py
@@ -1,8 +1,8 @@
 class event:
-    t0j = 0
-    t1j = 0
-    Lj = 0
-    incomingActions = [] #akcje wchodzace do zdarzen
-    outgoingActions = [] #akcje wychodzace ze zdarzen
     def __init__(self, ID):
+        self.t0j = 0
+        self.t1j = 0
+        self.Lj = 0
+        self.incomingActions = []  # akcje wchodzace do zdarzen
+        self.outgoingActions = []  # akcje wychodzace ze zdarzen
         self.ID = ID
